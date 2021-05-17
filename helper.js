7fa3b6eeb19716c40518cc332864141684d31a24
Maciej Kosmela <alternatywnykosmos@gmail.com>
2021-05-17 18:36:21 +0200

    add hepler.js = supplementing the database with initial values

---
+++ helper.js
@@ -0,0 +1,48 @@
+// you need this file if you have deleted all data from the database
+
+// {
+//     "grading": [
+//       {
+//         "name": "A",
+//         "grade_description": "Great job!",
+//         "percent_from": "100",
+//         "percent_to": "100",
+//         "id": 1
+//       },
+//       {
+//         "name": "B",
+//         "grade_description": "Very goog!",
+//         "percent_from": "90",
+//         "percent_to": "99",
+//         "id": 2
+//       },
+//       {
+//         "name": "C",
+//         "grade_description": "Pretty good!",
+//         "percent_from": "73",
+//         "percent_to": "89",
+//         "id": 3
+//       },
+//       {
+//         "name": "D",
+//         "grade_description": "Could be better!",
+//         "percent_from": "50%",
+//         "percent_to": "72%",
+//         "id": 4
+//       },
+//       {
+//         "name": "E",
+//         "grade_description": "You need to work more!",
+//         "percent_from": "31",
+//         "percent_to": "49",
+//         "id": 5
+//       },
+//       {
+//         "name": "F",
+//         "grade_description": "Generalie misery!",
+//         "percent_from": "0",
+//         "percent_to": "30",
+//         "id": 6
+//       }
+//     ]
+//   }
